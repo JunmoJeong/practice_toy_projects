167691593da0b83e81249dc0ab13a79d461c8044
JunmoJeong <jjmstars7@gmail.com>
2022-09-25 09:27:27 +0900

    qrcode gen

--- QRcode/qrmaker.py
+++ QRcode/qrmaker.py
@@ -1,5 +1,8 @@
 import pyqrcode
 
-qrcode = pyqrcode.create("https://www.naver.com")
-qrcode.svg("qr_naver.svg")
-qrcode.eps("qr_naver.eps")
+
+# """
+# qrcode = pyqrcode.create("https://www.naver.com")
+# qrcode.svg("qr_naver.svg")
+# qrcode.eps("qr_naver.eps")
+# """
